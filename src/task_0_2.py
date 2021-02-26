bf712725c7458aae21a1792c671a9507f2a08817
1xdeadman <daniilZakharchenko@gmail.com>
2021-02-26 11:37:57 +1000

    fix linux launch

--- src/task_0_2.py
+++ src/task_0_2.py
@@ -3,7 +3,8 @@
 Перед каждым <YOUR CODE> вы можете прочитать описание задания, помеченный фразой <TODO>.
 для проверки введите из папки с проектом: pytest tests/test_task_0_2.py
 """
+import os
 
 text = "For the Alliance!"
 # TODO: выведите значение переменной text в консоль
-"<YOUR CODE>"("<YOUR CODE>")
+print(text)
